cdd4407621e40bef6dad5237fe7f2a8fd2ec3f3e
siftab <siiffuuu@gmail.com>
2024-08-30 16:50:40 +0600

    Initial commit

--- src/app/modules/semesterRegestration/semsterRegistration.model.ts
+++ src/app/modules/semesterRegestration/semsterRegistration.model.ts
@@ -1,14 +1,42 @@
 import { Schema } from "mongoose";
 import { TsemesterRegistration } from "./semesterRegestration.interface";
+import { semesterRegistrationStatus } from "./semesterRegistratio.constant";
 
 
 
 
 
-const semesterRegistrationSchema = new Schema<TsemesterRegistration>({
+export const semesterRegistrationSchema = new Schema<TsemesterRegistration>({
 academicSemester:{
     type: Schema.Types.ObjectId,
     required:true,
-    ref: "AcademicSemester"
+    ref: "AcademicSemester",
+    unique:true 
+},
+status:{
+    type:String,
+    enum:semesterRegistrationStatus[Symbol],
+    default:"UPCOMING",
+    
 }
+,
+startDate:{
+    type:Date,
+    required: true 
+}
+,
+endDate:{
+    type:Date,
+    required: true 
+},
+minCredit:{
+    type:Number,
+    default:3
+},
+maxCredit:{
+    type:Number,
+    default:10
+}
+},{
+    timestamps:true
 })
