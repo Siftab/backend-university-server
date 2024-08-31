3bd26e998e7d2c4435e491ba5d963ff8dc4b9892
Siftab <siiffuuu@gmail.com>
2024-08-31 14:26:41 +0600

    Initial commit

--- src/app/modules/semesterRegestration/semesterregestration.services.ts
+++ src/app/modules/semesterRegestration/semesterregestration.services.ts
@@ -1,3 +1,6 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
+import { AcademicSemester } from "../academicSemester/academicSemester.model";
 import { TsemesterRegistration } from "./semesterRegestration.interface";
 import { SemesterRegistration } from "./semsterRegistration.model";
 
@@ -6,12 +9,21 @@ import { SemesterRegistration } from "./semsterRegistration.model";
 
 
 const createSemesterServiceIntoDb = async (payload:TsemesterRegistration)=>{
+ 
+    const academicSemester = payload?.academicSemester;
+
+    if(academicSemester){
+        const isAcademicSemesterExist = await AcademicSemester.findById(academicSemester)
+
+        if(!isAcademicSemesterExist){
+            throw new AppError(httpStatus.NOT_FOUND,"this academic semester not found ")
+        }
+
+    }
 
 
-  const result = await SemesterRegistration.create(payload)
 
-
-  return result
+//   return result
 
 }
 
