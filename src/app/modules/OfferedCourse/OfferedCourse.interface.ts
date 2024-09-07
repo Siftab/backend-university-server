0a949c36715ca2ac6027795fecd48be497ca60a3
Siftab <siiffuuu@gmail.com>
2024-09-07 10:42:13 +0600

    offeredCourse interface

---
+++ src/app/modules/OfferedCourse/OfferedCourse.interface.ts
@@ -0,0 +1,19 @@
+import { Types } from "mongoose";
+
+type Days = 'MON' | 'TUE' | 'WED' | 'THU' | 'FRI' | 'SAT' | 'SUN';
+
+
+export type TOfferedCourse = {
+    semesterRegistration : Types.ObjectId,
+    academicSemester :Types.ObjectId,
+    academicFaculty :Types.ObjectId,
+    academicDepartment:Types.ObjectId,
+    course:Types.ObjectId,
+    faculty:Types.ObjectId,
+    maxCapacity :number,
+    section:number,
+    days:Days,
+    startTime:string,
+    endTime:string 
+
+}
